a8900cc28f858e0d111c610836550a29d99728c2
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2016-04-11 18:39:15 +0200

    Start rewriting using AmigaOS API.

--- src/load2mem.c
+++ src/load2mem.c
@@ -1,50 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
 
-void
-usage(char *progname)
-{
-	printf("usage: %s filename [address]\n", progname);
-}
+#include <exec/types.h>
+#include <dos/dos.h>
 
-void
-prepare_filebuf(char **fb, long size)
-{
-	*fb = (char*) malloc(size);
-}
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#define EXIT_DOS_ERROR 30
 
 int
 main(int argc, char *argv[])
 {
-	int fd; 
-	struct stat statbuf;
-	char *filebuf;
+	struct RDArgs *result;
+/*
+	CONST_STRPTR argTemplate =
 
-	if (argc <= 1) {
-		usage(argv[0]);
-		exit(1);
-	}
-
-	if ((fd = open(argv[1], O_RDONLY)) == -1)  {
-		perror("Error openinig file");
-		exit(2);
-	}
-
-	fstat(fd, &statbuf);
-
-	if (argc == 3)
-		filebuf = (char*) strtol(argv[2], NULL, 16);
-	else 
-		prepare_filebuf(&filebuf, statbuf.st_size);
+	filebuf = (char*) strtol(argv[2], NULL, 16);
 
 	printf("file is %ld bytes long, will load at %p\n", (long) statbuf.st_size, filebuf);
+*/
+}
 
-	if (read(fd, filebuf, statbuf.st_size) == -1) {
-		perror("Error reading file");
-		exit(3);
+ULONG
+file_size(BYTE *path)
+{ 
+	BPTR file;
+	LONG size;
+	struct FileInfoBlock *fib;
+	struct Library *DOSBase;
+
+	DOSBase = OpenLibrary("dos.library", 36L);
+	if (!DOSBase) {
+		printf("Error opening dos.library!\n");
+		exit(EXIT_DOS_ERROR);
 	}
+
+	fib = (struct FileInfoBlock *) AllocDosObject(DOS_FIB, TAG_END);
+	if (!fib) {
+		printf("Couldn't allocate dos object!\n");
+
+		CloseLibrary(DOSBase);
+		exit(EXIT_DOS_ERROR);
+	}
+
+	if (file = Lock(path, SHARED_LOCK)) {
+
+		if (Examine(file, fib)) 
+			size = fib->fib_Size;
+		else
+			printf("Couldn't Examine() file!\n"); /* XXX */
+
+		UnLock(file);
+	} else {
+		printf("Couldn't lock file!\n");
+
+		FreeDosObject(DOS_FIB, fib);
+		CloseLibrary(DOSBase);
+		exit(EXIT_DOS_ERROR);
+	}
+
+	FreeDosObject(DOS_FIB, fib);
+	CloseLibrary(DOSBase);
+	
+	return size;
+}
+
+/* load file to memory buffer */
+BOOL
+file_load(BYTE *path, BYTE *filebuf, ULONG filesize)
+{
+	BPTR fh;
+
+	if ((fh = Open(path, MODE_OLDFILE)) == -1)  {	
+		perror("Error openinig file");
+		return FALSE;
+	}
+
+	printf("DEBUG: loading %lx bytes long file at %p\n", (ULONG) filesize, (void*) filebuf);
+
+	if (Read(fh, filebuf, filesize) == -1) {
+		perror("Error reading file");
+
+		Close(fh);
+		return FALSE;
+	}
+
+	Close(fh);
+
+	return TRUE;
 }
 
